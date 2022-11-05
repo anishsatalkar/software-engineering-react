e7f1a03ce2fcafde09937b9b9f123dab03df567f
Anish Satalkar <anishsatalkar@gmail.com>
2022-11-05 17:43:21 -0400

    Updated Node server URL to my remotely deployed server.

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-// const BASE_URL = "https://satalkar.herokuapp.com"
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://satalkar.herokuapp.com"
 const TUITS_API = `${BASE_URL}/api/tuits`;
 const USERS_API = `${BASE_URL}/api/users`;
 
--- src/services/users-service.js
+++ src/services/users-service.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-// const BASE_URL = "https://satalkar.herokuapp.com";
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://satalkar.herokuapp.com";
 
 const LOGIN_API = `${BASE_URL}/api/login`;
 const USERS_API = `${BASE_URL}/api/users`;
