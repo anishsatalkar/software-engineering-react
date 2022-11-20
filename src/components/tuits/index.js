e1749cc6085e424d9f0a0f8c7dd98a4c25c0c734
Anish Satalkar <anishsatalkar@gmail.com>
2022-11-20 17:54:11 -0500

    Working on like refresh.

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -1,19 +1,32 @@
 import Tuit from "./tuit";
 import * as likesService from "../../services/likes-service";
-const Tuits = ({tuits = [], deleteTuit,
-                   refreshTuits}) => {
+import * as tuitService from "../../services/tuits-service";
+import {useEffect, useState} from "react";
+const Tuits = ({tuits = [], deleteTuit}) => {
+    const [tuitsState, setTuits] = useState(tuits);
+    useEffect(() => setTuits(tuits));
+
+    const refreshTuits = async (tuit) => {
+        const foundTuit = await tuitService.findTuitById(tuit._id);
+        const tuitsInList = tuits.filter((element) => element._id === foundTuit._id);
+        if (!tuitsInList) return;
+        if (tuitsInList.length > 1) return;
+        const tuitInList = tuitsInList[0];
+        tuits[tuits.indexOf(tuitInList)] = foundTuit;
+        setTuits(tuits);
+    }
 
     const likeTuit = (tuit) =>
         likesService
             .userTogglesTuitLikes("me", tuit._id)
-            .then(refreshTuits)
+            .then(refreshTuits(tuit))
             .catch(e => alert(e))
 
     return (
         <div>
             <ul>
                 {
-                    tuits.map(tuit =>
+                    tuitsState.map(tuit =>
                         <Tuit key={tuit._id}
                               deleteTuit={deleteTuit}
                               likeTuit={likeTuit}
--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -2,18 +2,18 @@ const TuitStats = ({tuit, likeTuit}) => {
     return (
         <div className="row">
             <div className="col">
-        <span onClick={() => likeTuit(tuit)}>
-        {
-            tuit.stats.likes > 0 &&
-            <i className="fas fa-heart"
-               style={{color: 'red'}}></i>
-        }
-            {
-                tuit.stats.likes <= 0 &&
-                <i className="far fa-heart"></i>
-            }
-            {tuit.stats && tuit.stats.likes}
-        </span>
+                <span onClick={() => likeTuit(tuit)}>
+                    {
+                        tuit.stats.likes > 0 &&
+                        <i className="fas fa-heart"
+                           style={{color: 'red'}}></i>
+                    }
+                    {
+                        tuit.stats.likes <= 0 &&
+                        <i className="far fa-heart"></i>
+                    }
+                    {tuit.stats && tuit.stats.likes}
+                </span>
             </div>
         </div>
     );
